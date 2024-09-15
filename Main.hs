9d00d57ad56230cac824ec38d87963c32f355ffc
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2024-09-15 11:27:33 -0400

    Add 'closed form' for recurrence

--- Main.hs
+++ Main.hs
@@ -3,11 +3,25 @@ module Main where
 import Data.Bits (xor)
 import Test.LeanCheck.Utils.Operators ((|||))
 
+import Test.Hspec
+import qualified Test.Hspec.LeanCheck as LC
+
+import Debug.Trace (trace)
+
+trace' :: Show a => String -> a -> a
+trace' s x = trace (s ++ ": " ++ show x) x
+
+-- | Recurrence.
 f :: Double -> Double -> Double -> Double -> Double
 f a b c x = x - c * (x ** a - 1) * (x ** b - 1)
 
+-- | List of all values of recurrence starting from x0.
 g :: Double -> Double -> Double -> Double -> [Double]
 g a b c = iterate (f a b c)
+
+-- | Nth value of recurrence.
+h :: Double -> Double -> Double -> Double -> Int -> Double
+h a b c x n = g a b c x !! n
 
 xnor :: Bool -> Bool -> Bool
 xnor u v = not (u `xor` v)
@@ -32,6 +46,21 @@ gsearch a x0 (lb, ub) = bsearch (diverges . g') lb ub
 -- 4 - 1/3
 -- 5 - 1/4
 
+lhs :: Double -> Double -> Double -> Double
+lhs a b x = (x**a - 1) * (x**b - 1)
+
+rhs :: Double -> Double -> Double
+rhs d x = (x - 1)**d
+
 main :: IO ()
 main = do
   mapM_ print $ take 50 $ g 0.5 0.5 0.2 6
+  hspec $ do
+    describe "thing" $ do
+      it "has a lower bound" $
+        LC.property $ \a b ->
+          let a' = trace' "a" $ 1 / (abs a + 1.0000001)
+              b' = trace' "b" $ 1 / (abs b + 1.0000001)
+           in LC.exists 100000 $ \d -> LC.holds 100 $ \x ->
+             let x' = abs x + 1.0000001
+              in lhs a' b' x' >= rhs d x'
