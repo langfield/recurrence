8bf925795f4fbc52bf1dc05c2a9b1b528ddc7645
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2024-09-15 10:24:04 -0400

    Add a function to search for `c`

--- Main.hs
+++ Main.hs
@@ -1,19 +1,31 @@
 module Main where
 
-import Test.Hspec
-import Test.LeanCheck ((==>))
-import qualified Test.Hspec.LeanCheck as LC
-
-import Debug.Trace (trace)
-
-trace' :: Show a => String -> a -> a
-trace' s x = trace (s ++ ": " ++ show x) x
+import Data.Bits (xor)
+import Test.LeanCheck.Utils.Operators ((|||))
 
 f :: Double -> Double -> Double -> Double -> Double
 f a b c x = x - c * (x ** a - 1) * (x ** b - 1)
 
 g :: Double -> Double -> Double -> Double -> [Double]
 g a b c = iterate (f a b c)
+
+xnor :: Bool -> Bool -> Bool
+xnor u v = not (u `xor` v)
+
+bsearch :: Fractional a => (a -> Bool) -> a -> a -> [a]
+bsearch p a b
+  | p a `xnor` p b = []
+  | p m            = a : bsearch p a m
+  | otherwise      = a : bsearch p m b
+  where
+    m = (a + b) / 2
+
+diverges :: [Double] -> Bool
+diverges = (isInfinite ||| isNaN) . (!! 10000)
+
+gsearch :: Double -> Double -> (Double, Double) -> [Double]
+gsearch a x0 (lb, ub) = bsearch (diverges . g') lb ub
+  where g' c = g a a c x0
 
 -- 2 - 1
 -- 3 - 1/2
@@ -22,19 +34,4 @@ g a b c = iterate (f a b c)
 
 main :: IO ()
 main = do
-  print $ take 1000 $ g 1 1 1 2
-  hspec $ do
-    describe "g" $ do
-      it "converges" $ do
-        LC.propertyFor 1000 $ \a b c x ->
-          let a' = trace' "\na" $ 1 / (abs a + 1)
-              b' = trace' "b"   $ 1 / (abs b + 1)
-              c' = trace' "c"   $ abs c
-              x0 = trace' "x0"  $ abs x + 1
-              xn = trace' "xn"  $ g a' b' c' x0 !! 10000
-           in 
-              x0 > 1
-           && c < 1 / (x0 - 1)
-           && all (>0) [a',b',c']
-           && not (any isInfinite [a',b',c',x0])
-             ==> abs (1 - xn) < 0.1
+  mapM_ print $ take 50 $ g 0.5 0.5 0.2 6
